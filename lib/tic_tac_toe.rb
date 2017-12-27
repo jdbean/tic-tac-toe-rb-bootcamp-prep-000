f383f78dbd65e729006cb00a8dfb27e879159773
J.D. Bean <jonathandbean@gmail.com>
2017-12-27 23:09:17 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -66,3 +66,22 @@ end
 def current_player(board)
   return turn_count(board) % 2 == 0 ?  "X" :  "O"
 end
+
+
+def full?(board)
+  board.all?{|square| square == "X" || square == "O"}
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  full?(board) || draw?(board) || won?(board)
+end
+
+def winner(board)
+  if won?(board)
+    return board[won?(board)[0]]
+  end
+end
