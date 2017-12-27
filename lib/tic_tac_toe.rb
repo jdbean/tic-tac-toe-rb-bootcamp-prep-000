e8fcfdcff5e297943aca41fb69f021896e9eee9a
J.D. Bean <jonathandbean@gmail.com>
2017-12-27 22:57:32 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -27,7 +27,7 @@ def move(board, index, player)
 end
 
 def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " " || board[index == ""])
+  !(board[index].nil? || board[index] == " " || board[index] == "")
 end
 
 def valid_move?(board, index)
