ebd1405ca980d3220c915ce9d30992d25c22f2f6
J.D. Bean <jonathandbean@gmail.com>
2017-12-27 23:07:59 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,6 +52,17 @@ def turn(board)
   end
 end
 
+
 def turn_count(board)
-  return board.length
+  turns_taken = 0
+  board.each do |board|
+    if board == "X" || board == "O"
+      turns_taken += 1
+    end
+  end
+  return turns_taken
+end
+
+def current_player(board)
+  return turn_count(board) % 2 == 0 ?  "X" :  "O"
 end
